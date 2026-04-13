3aa0ac83ba2c6dfa0d01a691c3589ea0bc9239ff
wind86861-lab <wind86861@gmail.com>
2026-04-13 11:48:56 +0500

    fix: correct down_revision in migration 005 to 004_seed_regions

--- migrations/versions/005_add_zavod.py
+++ migrations/versions/005_add_zavod.py
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 revision: str = '005'
-down_revision: Union[str, None] = '004'
+down_revision: Union[str, None] = '004_seed_regions'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
